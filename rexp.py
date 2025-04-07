079f39296e8956700761fad4a4c9ae0ddeb29fbd
naaatami <144642305+naaatami@users.noreply.github.com>
2025-04-07 18:14:38 -0500

    tiny rename + grabbing RE from args

--- rexp.py
+++ rexp.py
@@ -1,1 +1,51 @@
+import re
+import sys
+
+class NFA:
+    def __init__(self):
+        self.states = {}
+        self.acceptingStates = []
+        self.initialState = 0
+        self.nodeCount = 0
+        self.currentNode = 0
+
+    def addTransition(self,transition,endState):
+        self.states.update({transition: endState})
+
+    def addLiteral(self,literal):
+        if self.nodeCount > 0:
+            self.states.update({(self.currentNode,""):self.currentNode+1})
+            self.nodeCount+=1
+            self.currentNode+=1
+            
+        self.states.update({(self.currentNode, literal):self.currentNode+1})
+        self.nodeCount+=1
+        self.currentNode+=1
+    
+    def addStarClosure(self,literals):
+        #startNode = self.currentNode
+        #for literal in literals:
+            #self.addLiteral(literal)
+        pass
+        
+    def addAlternation(self):
+        pass
+
+    
+    #def addStarClosure(inNode,)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        reg = sys.argv[1]
+    else:
+        print("No regular expression entered, defaulting to ab*a|a(ba)*")
+        reg = "ab*a|a(ba)*"
+
+    dingus = NFA()
+    dingus.addLiteral("a")
+    dingus.addLiteral("b")
+    print(dingus.states)
+
+
 
